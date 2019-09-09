8eb66455fcff9ea888a79574bf3f0192a2a058d8
Luca Braglia <lbraglia@gmail.com>
2019-09-09 17:28:56 +0200

    month to dnd

---
+++ R/month_to_cv.R
@@ -0,0 +1,21 @@
+#' funzione che trasforma un mese numerico nel nome in CV
+#'
+#' funzione che trasforma un mese numerico nel nome in CV
+#' 
+#' @param x number of month
+#' @export
+month_to_cv <- function(x){ 
+    mesi   <- c("Hammer",                   #  1
+                "Alturiak",                 #  2
+                "Ches",                     #  3
+                "Tarsakh",                  #  4
+                "Mirtul",                   #  5
+                "Kythorn",                  #  6
+                "Flamerule",                #  7
+                "Eleasis",                  #  8
+                "Eleint",                   #  9
+                "Marpenoth",                # 10
+                "Uktar",                    # 11
+                "Nightal")                  # 12
+    mesi[x]
+}
