52cbacdd352b2d20367689daac639ec7888984fc
Luca Braglia <lbraglia@gmail.com>
2019-09-11 11:36:36 +0200

    update

--- R/dividi_loot.R
+++ R/dividi_loot.R
@@ -10,6 +10,6 @@
 dividi_loot <- function(MO = 0, MA = 0, MR = 0, MP = 0, ME = 0, n = 6) {
     tot_in_MO <- (MP * 10) + MO + (ME / 2) + (MA / 10) + (MR / 100)
     c('pg' = n,
-      "MO a testa" = floor(tot_in_MO/n),
+      "MO a testa" = floor(tot_in_MO / n),
       "resto in MO" = tot_in_MO %% n)
 }
