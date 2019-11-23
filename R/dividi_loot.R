7769fb42adf1b87e84d9f5b2ecf12260d6413c70
Luca Braglia <lbraglia@gmail.com>
2019-11-23 18:07:03 +0100

    update

--- R/dividi_loot.R
+++ R/dividi_loot.R
@@ -7,7 +7,7 @@
 #' @param ME monete elettro
 #' @param n numero di PG per i quale fare la ripartizione
 #' @export
-dividi_loot <- function(MO = 0, MA = 0, MR = 0, MP = 0, ME = 0, n = 6) {
+dividi_loot <- function(MO = 0, MA = 0, MR = 0, MP = 0, ME = 0, n = 7) {
     tot_in_MO <- (MP * 10) + MO + (ME / 2) + (MA / 10) + (MR / 100)
     c('pg' = n,
       "MO a testa" = floor(tot_in_MO / n),
